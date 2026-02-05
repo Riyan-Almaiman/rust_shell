344a163f42d62c2a726b83c9c581e7f3218261a9
Riyan Almaiman <ralmaiman@gmail.com>
2026-02-05 14:46:45 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,27 @@ use std::io::{self, Write};
 
 
 fn main() {
-    loop {
-     let mut command = String::new();
 
-    print!("$ ");
-    io::stdout().flush().unwrap();
-    
-    io::stdin().read_line(&mut command).unwrap();
-    
-    let command = command.trim();
-    match command {
-        "exit" => break,
-        _ => println!("{}: command not found", command.trim())
-    }}
+    let mut exit = false;
+    while !exit {
+        let mut command = String::new();
+
+        print!("$ ");
+        io::stdout().flush().unwrap();
+
+        io::stdin().read_line(&mut command).unwrap();
+
+        let command = command.trim();
+  
+        match command {
+            "exit" => exit = true,
+            _ => println!("{}: command not found", command.trim())
+        }
+  }
+}
+
+fn echo(message: &str ){
+
+    println!("{}", message);
+    break;
 }
