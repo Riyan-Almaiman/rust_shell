7d5a8e57de7c91e0a7c01fab6320b280e6202041
riyan-almaiman <ralmaiman@gmail.com>
2026-02-05 23:44:19 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 
-use std::{env, ffi::{OsStr, OsString} };
+use std::{env, ffi::{ OsString}, path::PathBuf };
 use is_executable::{is_executable};
 
 #[allow(unused_imports)]
@@ -9,16 +9,26 @@ enum ShellAction {
     Exit,
 }
 
-#[derive(Debug)]
-struct CommandInput {
-    command: CommandType,
-    command_str: String,
-    args: Vec<String>,
-    raw_args: String,
-    
+struct CommandInput<'a> {
+    command_type: CommandType,
+    command_str: &'a str,   
+    args: Vec<&'a str>,    
+    raw_args: &'a str,      
 }
-#[derive(Debug)]
+impl<'a> CommandInput<'a>{
+    fn execute(&self) -> ShellAction{
 
+        let mut process = std::process::Command::new(self.command_str)
+            .args(&self.args)
+            .spawn().expect("failed to spawn process");
+
+        process.wait().expect("failed to wait for process");
+
+        return ShellAction::Continue;
+
+    }
+}
+#[derive(PartialEq)]
 enum CommandType {
     Exit , 
     Echo,
@@ -29,33 +39,40 @@ enum CommandType {
 
 impl CommandType {
 
-    fn parse( input: &str, paths: &OsStr) -> CommandType {
+    fn parse( input: &str) -> CommandType {
         match input {
             "exit" => CommandType::Exit,
             "echo" => CommandType::Echo,
              "type"=> CommandType::Type,
-            _ => CommandType::parse_unknown(&input, &paths)
+            _ => CommandType::Unknown
         }
     }
 
-
-    fn parse_unknown(command: &str,paths:&OsStr) -> CommandType{
-
-        let paths_split = env::split_paths(paths);
-        for path in paths_split {
+    fn parse_unknown(command: &str, paths: &[PathBuf]) -> CommandType{
+    
+        for path in paths {
             let file = path.join(command );
             if is_executable(&file){
                 return CommandType::Exec {name:command.to_string(), path: file.into_os_string()};
             }
         }
         CommandType::Unknown
-}
+    }
+    fn parse_type(command: &str, paths: &[PathBuf]) -> CommandType{
+        
+        let cmd_type = match CommandType::parse(command){
+            CommandType::Unknown=> CommandType::parse_unknown(command, paths),
+            builtin=> builtin
+
+        };
+        cmd_type
+    }
 }
 
 fn main() {
 
     let key = "PATH";
-    let paths =  std::env::var_os(key).unwrap_or(OsString::from(""));
+    let paths: Vec<PathBuf> = env::split_paths(&std::env::var_os(key).unwrap_or(OsString::from(""))).collect();
     
     loop {
         let mut input = String::new();
@@ -73,25 +90,25 @@ fn main() {
                 .split_once(' ')
                 .unwrap_or((input, ""));
             
+        let type_of_command = match CommandType::parse(command) {
+                CommandType::Unknown => CommandType::parse_unknown(command, &paths),
+                cmd =>  cmd
 
-        let  input_tokens =
-             args
-             .split_whitespace()
-            .map(String::from);
-
+        };
+  
         let command_input = CommandInput {
-            command_str: command.to_string(),
-            command: CommandType::parse(command, &paths),
-            args: input_tokens.collect(),
-            raw_args: args.to_string(),
+            command_str: command,
+            command_type: type_of_command ,
+            args:  args.split_whitespace().collect(),
+            raw_args: args,
         };
 
-        let action = match command_input.command {
+        let action = match command_input.command_type {
 
             CommandType::Exit => ShellAction::Exit,
-            CommandType::Type=> type_command(&command_input.args.get(0).unwrap_or(&"".to_string()),  &paths),
+            CommandType::Type=> type_command(&command_input.args.get(0).map_or("", |v| v),  &paths),
             CommandType::Echo =>  echo(&command_input.raw_args),
-            CommandType::Exec { .. } => execute( &command_input),
+            CommandType::Exec { .. } => command_input.execute(),
             CommandType::Unknown  => command_not_found(&command_input.command_str)
         };
 
@@ -104,25 +121,14 @@ fn main() {
 }
 
 
-fn execute( command_input: &CommandInput) -> ShellAction{
 
-  
-    let mut process = std::process::Command::new(command_input.command_str.clone())
-        .args(command_input.args.clone())
-        .spawn().expect("failed to spawn process");
-
-     process.wait().expect("failed to wait for process");
-
-    return ShellAction::Continue;
-
-}
-fn type_command(command: &str, paths: &OsStr)->ShellAction{
+fn type_command(command: &str, paths:&[PathBuf] )->ShellAction{
     if command.is_empty(){
         println!("No command found");
         return ShellAction::Continue
     }
     
-    match CommandType::parse(command, paths){
+    match CommandType::parse_type(command, paths){
         CommandType::Unknown =>  println!("{}: not found", command),
         CommandType::Exec { path, name } => println!("{} is {}", name, path.display()),
         _ => println!("{} is a shell builtin", command)
