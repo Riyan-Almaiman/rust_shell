8f65819cad09725c85553bd2ec6c4050768c252c
Riyan Almaiman <ralmaiman@gmail.com>
2026-02-05 15:40:06 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -3,28 +3,47 @@ use std::{fs::read_dir, io::Read};
 use std::io::{self, Write};
 
 
+
 fn main() {
 
+  
     let mut exit = false;
     while !exit {
-        let mut command = String::new();
+        let mut input = String::new();
 
         print!("$ ");
         io::stdout().flush().unwrap();
 
-        io::stdin().read_line(&mut command).unwrap();
+        io::stdin().read_line(&mut input).unwrap();
+        let input = input.trim();
 
-        let command = command.trim();
-  
-        match command {
-            "exit" => exit = true,
-            _ => println!("{}: command not found", command.trim())
+        if input.trim().is_empty() {
+            println!("No Command Found");
+            continue;
+        }
+        let command_string: Vec<String> = input.split(' ').map(String::from).collect();
+ 
+    
+        let command = &command_string[0];
+        let command_args = &command_string[1..];
+        
+        exit = match command.as_str() {
+
+            "exit" => true,
+            "echo" =>  echo(command_args),
+            _ => command_not_found(command)
         }
   }
 }
 
-fn echo(message: &str ){
+fn command_not_found(command: &str) -> bool {
 
-    println!("{}", message);
-    break;
+    println!("{}: command not found", command);
+    false
+}
+
+fn echo(message: &[String] )-> bool{
+
+    println!("{}", message.join(" "));
+    true
 }
